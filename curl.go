262000674d794fef5bca7dccbe38cb4d513602af
Lin Chen <lc1990linux@gmail.com>
2020-01-17 12:07:01 +0800

    master add curl and fetch all v curl will retrieve url one by one v fetchAll using goroutine will get urls all together

---
+++ curl.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	for _, url := range os.Args[1:] {
+		s := time.Now()
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			return
+		}
+		nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+		resp.Body.Close()
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			return
+		}
+		secs := time.Since(s).Seconds()
+		fmt.Printf("%.2fs %7d %s\n", secs, nbytes, url)
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
