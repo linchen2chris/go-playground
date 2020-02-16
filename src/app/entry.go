33351c26da4850793bdf717d0a8b1bedafde0c9f
Lin Chen <lc1990linux@gmail.com>
2020-02-16 22:34:18 +0800

    [Package][Chen] package init func and import alias

--- src/app/entry.go
+++ src/app/entry.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"app/util"
-	"consts"
-	"consts/de"
+	_ "consts"
+	. "consts/de"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("app/entry =>> main")
 	fmt.Println(morning)
-	fmt.Println(consts.Greeting)
-	fmt.Println(de.Greet)
+	fmt.Println(Greet)
 	fmt.Println(util.Add(1, 2))
 }
--- src/consts/greet.go
+++ src/consts/greet.go
@@ -1,3 +1,10 @@
 package consts
 
+import "fmt"
+
 var Greeting = "hello"
+
+// init ...
+func init() {
+	fmt.Println("greet/parent => init()")
+}
